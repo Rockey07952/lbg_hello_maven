688cf2ebb420c87ef60174a2a2ef4bcd09e8ecdb
unknown <Ashley>
2026-04-28 16:15:28 +0100

    CREATE: Aoutomation of pipeline

--- src/main/java/com/qa/App.java
+++ src/main/java/com/qa/App.java
@@ -14,6 +14,7 @@ public class App
         System.out.println(sayHelloToSomeone("Charlie"));
         System.out.println(sayHelloToSomeone("Zena"));
         System.out.println(sayHelloToSomeone("Ashley"));
+        System.out.println(sayHelloToSomeone("Riz"));
         System.out.println(sayGoodbye());
     }
 
